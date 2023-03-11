085b7a387bcba496e86ddf8669b3a8c0b6f6c284
damir <bajrami.damir@gmail.com>
2023-03-11 14:31:26 +0100

    installed new packages

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-function App(): JSX.Element {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <>
-      
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 export default App;
